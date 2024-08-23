2532c764665f6e71cacfd0219bf029c86015cdd5
ANSHUMAAN SINGH <124035463+anshumaan-10@users.noreply.github.com>
2024-08-23 19:37:40 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,9 @@
 import os
 
-# Configuration settings for your application
 class Config:
     DEBUG = os.getenv('DEBUG', False)
     DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///app.db')
-    # Add other configuration settings as needed
+    SCAN_PORT = int(os.getenv('SCAN_PORT', 8888))  # Set default port to 8888
 
 # Additional environment-specific configurations
 class DevelopmentConfig(Config):
